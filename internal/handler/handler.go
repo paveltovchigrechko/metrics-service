e9d4ac1da04f614a03a3dd22b02ee00f45ae3852
paveltovchigrechko <pavel.tovchigrecko.1988@gmail.com>
2026-06-24 15:02:00 +0600

    Add 400 http code for wrong endpoint

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -31,12 +31,11 @@ func (h *AppHandler) MainPage(w http.ResponseWriter, req *http.Request) {
 		switch err.Error() {
 		case "No metric":
 			w.WriteHeader(http.StatusNotFound)
-		case "Invalid metric value", "Invalid metric name":
+		case "Invalid metric value", "Invalid metric name", "Invalid endpoint":
 			w.WriteHeader(http.StatusBadRequest)
 		case "Invalid path":
 			w.WriteHeader(http.StatusNotFound)
-		case "Invalid endpoint":
-			break
+
 		}
 		w.Write([]byte(err.Error())) // Delete
 		return
